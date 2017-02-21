c4bb81bcb0b528e7e22a32742c5fcb76272d5c47
Vladislav <vladislav.voicehovich@gmail.com>
2017-02-21 00:04:23 +0000

    displayPrimesDivideUpToRoot finished. Not the most efficient algorithm (use to compare time + validate correctness of primes)

--- public_html/primes.js
+++ public_html/primes.js
@@ -6,9 +6,37 @@
 
 
 
+function displayPrimesDivideUpToRoot(){//1,000,000 primes (largest 15,485,863), 13.384 seconds without printing
+	
+	var start = new Date().getTime();
+	
+	var n = document.getElementById("inField").value;
+	var limit; 
+	var nextToCheck = 1;
+	var allPrimes = [];
+	
+	outerLoop:
+	while(n>0){
+		nextToCheck++;
+		limit = Math.floor(Math.sqrt(nextToCheck));
+		
+		for (var i=2; i<=limit;i++){
+			if (nextToCheck % i === 0){
+				continue outerLoop;
+			}
+		}
+		
+		n--;
+		allPrimes.push(nextToCheck);
+	}
+	
+	console.log(allPrimes[allPrimes.length-1]);
+	console.log("time ms:"+((new Date().getTime())-start));
+	
+};
 
 
 
-function helloWorld() {
-  return "Hello world!";
-}
+
+
+
